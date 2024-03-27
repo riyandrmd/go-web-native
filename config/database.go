30a410690b71c5d129400c37deed29683b52b85c
riyandrmd <ryanda.rama10@gmail.com>
2024-03-27 10:27:41 +0700

    fix error

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	db, err := sql.Open("mysql", "root:@/go_products")
+	db, err := sql.Open("mysql", "root:@/go_products?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
