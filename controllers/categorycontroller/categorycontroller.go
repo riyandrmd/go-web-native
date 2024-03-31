8a8870d731b485200d2aeb2a7eb854f450ecb21f
riyandrmd <ryanda.rama10@gmail.com>
2024-03-31 13:57:12 +0700

    create edit category

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -1,9 +1,13 @@
 package categorycontroller
 
 import (
+	"go-web-native/entities"
 	"go-web-native/models/categorymodel"
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -31,12 +35,64 @@ func Add(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "POST" {
+		var category entities.Category
 
+		category.Name = r.FormValue("name")
+		category.CreatedAt = time.Now()
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Create(category); !ok {
+			temp, _ := template.ParseFiles("views/category/create.html")
+			temp.Execute(w, nil)
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
 	}
 
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		temp, err := template.ParseFiles("views/category/edit.html")
+		if err != nil {
+			panic(err)
+		}
+
+		idString := r.URL.Query().Get("id")
+
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category := categorymodel.Detail(id)
+		data := map[string]any{
+			"category": category,
+		}
+
+		temp.Execute(w, data)
+	}
+
+	if r.Method == "POST" {
+		var category entities.Category
+
+		idString := r.FormValue("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category.Name = r.FormValue("name")
+		category.UpdatedAt = time.Now()
+		log.Println(time.Now())
+
+		if ok := categorymodel.Update(id, category); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 
 }
 
